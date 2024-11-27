1aaae17cc2e8d3fac1d6064d0326cb2528a6c4cb
Iury <iuryrbs@edu.unifor.br>
2024-11-27 15:08:48 -0300

    Componentes iniciais

---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+import '../styles/header.css'
+
+function Header(){
+    return (
+        <>
+            <header className='topo'>
+                <div className='logo'>
+                    <img src="" alt="Logo" />
+                    <a href="#">Unifor</a>
+                </div>
+                <a href="#" className='nome'>Chat CCT</a>
+            </header>
+        </>
+    )
+}
+
+export default Header
---
+++ src/components/mainContainer.tsx
@@ -0,0 +1,17 @@
+import Header from './header'
+import '../styles/mainContainer.css'
+
+function MainContainer(){
+    return (
+        <>
+            <div className="container">
+                <Header />
+                <div className="main">
+                    
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default MainContainer
